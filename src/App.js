35cb023c9534a4bb65c27102b170f31c73db0bb7
Khushal Sharma <sharmakhushal78@gmail.com>
2021-05-03 13:42:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Let's DOS the CoWin</h1>
+      <h2>#ModijiApiRateLimitKro</h2>
+    </div>
+  );
+}
