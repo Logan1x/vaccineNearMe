990fd28c757bf03b04d172ad7fc2fa6ab7031ee0
Khushal Sharma <sharmakhushal78@gmail.com>
2021-05-03 14:14:24 +0530

    learned about moment library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./styles.css";
+import moment from "moment";
 
 export default function App() {
+  var dateInEpochTS = 1595314414299;
+  var now = moment().format('DD-MM-YYYY');
+  console.log("NOW");
+  console.log(now);
+
   return (
     <div className="App">
       <h1>Let's DOS the CoWin</h1>
