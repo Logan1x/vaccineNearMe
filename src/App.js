dfdc8d2265dc7c78f37193d91e40df4718a8b55a
Khushal Sharma <sharmakhushal78@gmail.com>
2021-05-03 17:40:34 +0530

    final update with footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
-import Lookup from './lookup';
+import Lookup from "./lookup";
 
 export default function App() {
-
   return (
     <div className="App">
       <h1>Is there vaccine spot avaible near me?</h1>
       <Lookup />
+      <footer>
+        Developed by <a href="https://twitter.com/herkuch">Khushal</a> & <a href="https://twitter.com/Pandyaparthppp">Parth</a>
+      </footer>
     </div>
   );
 }
